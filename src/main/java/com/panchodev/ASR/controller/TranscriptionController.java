d244f9f001a0fafbccbab6ba3110ec95f7de2ffb
Francisco Ríos <panchorios_242@hotmail.com>
2023-07-22 19:30:08 -0400

    Added validation for Maximum file size exceeded

--- src/main/java/com/panchodev/ASR/controller/TranscriptionController.java
+++ src/main/java/com/panchodev/ASR/controller/TranscriptionController.java
@@ -5,12 +5,22 @@ import com.amazonaws.services.transcribe.model.StartTranscriptionJobResult;
 import com.panchodev.ASR.dto.TranscriptionResponseDTO;
 import com.panchodev.ASR.helpers.AwsBucket;
 import com.panchodev.ASR.service.TranscriptionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -29,21 +39,42 @@ public class TranscriptionController {
 
     private final TranscriptionService transcriptionService;
 
+    @Value("${spring.servlet.multipart.max-file-size}")
+    private String maxFileSize;
+
+
+    @Tag(name = "Transcription", description = "Transcription APIs")
+    @Operation(
+            summary = "Retrieve the content of an audio")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Transcription response", content = {@Content(schema = @Schema(implementation = TranscriptionResponseDTO.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "413", description = "File size exceeded", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json"))
+    })
     @PostMapping(value = "/extractFromFile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public TranscriptionResponseDTO extractSpeechTextFromVideo(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<?> extractSpeechTextFromVideo(@Parameter(description = "Audio file to analyze") @RequestPart("file") MultipartFile file) {
+
         log.info("Request to extract Speech Text from Video : {}", file.getContentType());
 
         if (file.isEmpty()) {
             // Handle the case when no file is sent
             // Return an error response or throw an exception
-            throw new IllegalArgumentException("No file sent");
+            return new ResponseEntity<>("You must select a file!", HttpStatus.BAD_REQUEST);
+        }
+
+        // Check file size
+        long fileSizeInBytes = file.getSize();
+        long maxFileSizeInBytes = Long.parseLong(maxFileSize); // 50 MB
+        if (fileSizeInBytes > maxFileSizeInBytes) {
+            return new ResponseEntity<>("File size exceeds the maximum allowed limit of " + maxFileSize + " MB.", HttpStatus.PAYLOAD_TOO_LARGE);
         }
 
         String contentType = file.getContentType();
         if (!isSupportedContentType(contentType)) {
             // Handle the case when the file is not an audio file
             // Return an error response or throw an exception
-            throw new IllegalArgumentException("Invalid file type. Only audio files are allowed.");
+            return new ResponseEntity<>("Invalid file type. Only audio files are allowed.", HttpStatus.BAD_REQUEST);
         }
 
         // Upload file to Aws
@@ -74,7 +105,7 @@ public class TranscriptionController {
         //Delete the transcription job after finishing, or it will get deleted after 90 days automatically if you do not call
         transcriptionService.deleteTranscriptionJob(transcriptionJobName);
 
-        return transcriptionResponseDTO;
+        return ResponseEntity.ok(transcriptionResponseDTO);
     }
 
 
